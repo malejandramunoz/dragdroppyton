da5e95f1cfc4b033db16ac48fc23483ded68b72f
malejandramunoz <maria.munoz10@upr.edu>
2021-12-06 19:55:47 -0400

    Added code that reads file created in frontend

--- lexerpaser/test.py
+++ lexerpaser/test.py
@@ -1,8 +1,11 @@
 import lexerpaser
 
 while True:
-    text = input('input  >')
+    with open("/Users/alejandramunoz/Downloads/code.txt") as content: 
+        text = content.read() 
+
     result, error = lexerpaser.run('<stdin>', text)
     
     if error: print(error.as_string())
     else: print(result)
+    
